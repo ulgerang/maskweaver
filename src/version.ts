2b04a77ce72aba1eb94f3a25dd5908b578064bfb
user <ulgerang@gmail.com>
2026-05-07 12:46:36 +0900

    Release maskweaver 0.11.1

--- src/version.ts
+++ src/version.ts
@@ -9,7 +9,7 @@
  * 2. Update this constant
  * 3. That's it ? all other files import from here
  */
-export const VERSION = '0.11.0';
+export const VERSION = '0.11.1';
 
 /**
  * Returns a formatted version string for display.
